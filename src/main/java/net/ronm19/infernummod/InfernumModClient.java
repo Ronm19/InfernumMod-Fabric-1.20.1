4c270b36e300c2fcfcbb27e0943cd2554b0c9f83
Ronm19 <herobrinemasterx777@gmail.com>
2025-08-20 17:31:00 +0300

    Initial commit

---
+++ src/main/java/net/ronm19/infernummod/InfernumModClient.java
@@ -0,0 +1,10 @@
+package net.ronm19.infernummod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfernumModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
